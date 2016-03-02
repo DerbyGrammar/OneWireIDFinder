aee7be5b4a717cf7e3af85937f507665d0bba1c4
ThorinDev <c.nethercott@icloud.com>
2016-03-02 10:26:58 +0000

    Update OneWireIDFinder.ino

--- src/OneWireIDFinder/OneWireIDFinder.ino
+++ src/OneWireIDFinder/OneWireIDFinder.ino
@@ -1,18 +1,7 @@
-/* YourDuino Example: Find Address of a DS18B20 Temperature Sensor
- Cut and paste the address to a text file for later use.
- V1.1 01/17/2013
- Questions: terry@yourduino.com 
- 
- Connections:
- DS18B20 Pinout (Left to Right, pins down, flat side toward you)
- - Left   = Ground
- - Center = Signal (Pin 2):  (with 3.3K to 4.7K resistor to +5 or 3.3 )
- - Right  = +5 or +3.3 V   
- This sketch looks for 1-wire devices and  prints their addresses (serial number)
- to the Serial Monitor in a format that is useful in Arduino sketches.
- Based on example at: 
- http://www.hacktronics.com/Tutorials/arduino-1-wire-address-finder.html
- */
+/*
+  Chris Nethercott
+  OneWireIDFinder
+*/
 
 /*-----( Import needed libraries )-----*/
 #include <OneWire.h>
